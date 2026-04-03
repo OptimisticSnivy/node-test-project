c9817318aae47220e01a6cc4dd6d1787b45973b4
Gopikaajindal <jindalgopikaa.com>
2026-04-03 12:19:26 +0530

    validation w/ middleware

---
+++ middleware/userValidator.js
@@ -0,0 +1,23 @@
+const Joi = require('joi')
+const myCustomJoi = Joi.extend(require('joi-phone-number'));
+
+const userSchema = Joi.object({
+	username: Joi.string().min(3).max(30).required(),
+	email: Joi.string().email().trim().required(),
+	password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{8,30}$')).required(),
+	city: Joi.string().trim().required(),
+	state: Joi.string().trim().required(),
+	country: Joi.string().trim().required(),
+	country_code: Joi.string().trim().required(),
+	phone_number: myCustomJoi.string().trim().phoneNumber().required(),
+})
+
+const validateUser = (req, res, next) => {
+	const { error } = userSchema.validate(req.body);
+	if (error) {
+		return res.status(400).send(error.details[0].message);
+	}
+	next();
+};
+
+module.exports = validateUser;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router();
+const userValidator = require('../middleware/userValidator')
 const userController = require('../controllers/userController')
 
-router.post('/', userController.createUser)
+router.post('/', userValidator, userController.createUser)
 
 router.get('/', userController.getAllUsers)
 router.get('/:id', userController.getUserById)
