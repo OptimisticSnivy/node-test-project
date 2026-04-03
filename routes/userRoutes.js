f999e4834656b9a6857c16d23a9062ee350cc950
Gopikaajindal <jindalgopikaa.com>
2026-04-03 10:49:35 +0530

    Revert "Email validates on the server."
    This reverts commit 1a8866709835b26377ec275804343b6fd4cbe2aa.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const { body } = require("express-validator")
 const router = express.Router();
 const userController = require('../controllers/userController')
 
@@ -8,7 +7,7 @@ router.post('/', userController.createUser)
 router.get('/', userController.getAllUsers)
 router.get('/:id', userController.getUserById)
 
-router.put('/:id', body('email').trim().isEmail(), userController.updateUser)
+router.put('/:id', userController.updateUser)
 
 router.delete('/:id', userController.deleteUser)
 
